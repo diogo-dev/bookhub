f8550db867f6304f70a308df2354291a885058bd
André Schlichting <andresch.dev@gmail.com>
2025-11-15 15:21:50 -0300

    Fix not null birth of author on migrate:down

--- server/src/migrations/1761061970040_make-author-birth-nullable.ts
+++ server/src/migrations/1761061970040_make-author-birth-nullable.ts
@@ -6,5 +6,6 @@ export const up = transaction(async (client: Client) => {
 });
 
 export const down = transaction(async (client: Client) => {
+  await client.query("UPDATE author SET birth_date = '1970-01-01' WHERE birth_date IS NULL;");
   await client.query("ALTER TABLE author ALTER COLUMN birth_date SET NOT NULL;");
 });
