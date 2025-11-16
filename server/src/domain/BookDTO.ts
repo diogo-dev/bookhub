4f530c3cc8ba06f624d7b13beba7578eb8ef7572
André Schlichting <andresch.dev@gmail.com>
2025-11-16 17:44:47 -0300

    Replace commas by semicolons

--- server/src/domain/BookDTO.ts
+++ server/src/domain/BookDTO.ts
@@ -1,21 +1,21 @@
 interface BookDTO {
-  readonly ISBN: string,
-  readonly workID: string | null,
-  readonly category: string | null,
-  readonly genres: string[],
-  readonly title: string,
-  readonly subtitle: string | null,
-  readonly description: string | null,
-  readonly cover: string | null,
-  readonly authors: string[],
-  readonly publisher: string | null,
-  readonly edition: string | null,
-  readonly language: string | null,
-  readonly numberOfPages: number,
-  readonly numberOfVisits: number,
-  readonly publishedAt: number | null,
-  readonly createdAt: number,
-  readonly items: string[]
+  readonly ISBN: string;
+  readonly workID: string | null;
+  readonly category: string | null;
+  readonly genres: string[];
+  readonly title: string;
+  readonly subtitle: string | null;
+  readonly description: string | null;
+  readonly cover: string | null;
+  readonly authors: string[];
+  readonly publisher: string | null;
+  readonly edition: string | null;
+  readonly language: string | null;
+  readonly numberOfPages: number;
+  readonly numberOfVisits: number;
+  readonly publishedAt: number | null;
+  readonly createdAt: number;
+  readonly items: string[];
 }
 
 export type { BookDTO };
