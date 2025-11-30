97b3292ac0f9e45f98224f73ddec2bed69c13993
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-11-30 16:19:54 -0300

    Added status field to BookItem constructor

--- server/src/domain/BookItem.ts
+++ server/src/domain/BookItem.ts
@@ -2,7 +2,8 @@ class BookItem {
   constructor(
     public readonly ISBN: string,
     public readonly ID: string = crypto.randomUUID(),
-    public readonly createdAt: number = Date.now()
+    public readonly createdAt: number = Date.now(),
+    public readonly status: "disponivel" | "emprestado" | "indisponivel" | "reservado" = "disponivel"
   ) {}
 }
 
