e2c1a56c28753133f1f77b210901779df489a486
André Schlichting <andresch.dev@gmail.com>
2026-01-28 12:18:50 -0300

    Update environment variables of server

--- server/src/infra/pg/connection.ts
+++ server/src/infra/pg/connection.ts
@@ -2,11 +2,7 @@ import "dotenv/config";
 import { Client } from "pg";
 
 const client = new Client({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_NAME
+  connectionString: process.env.DATABASE_URL
 });
 
 client.connect()
